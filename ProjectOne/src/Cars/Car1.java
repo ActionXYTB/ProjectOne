3c91820055b10e6c596fb0f726f0828f1364096c
SaiTheFlyGuy <SaiTheFlyGuy@DESKTOP-Q4UN17C.home>
2020-08-27 10:57:02 -0400

    initial commit

--- ProjectOne/src/Cars/Car1.java
+++ ProjectOne/src/Cars/Car1.java
@@ -1,5 +1,23 @@
 package Cars;
 
-public abstract class Car1 {
+
+public abstract class Car1 implements RollsRoyce {
+
+	public int maxSpeed; 
+	 
+	
+	public void start() {
+		
+		System.out.println("starting engine...");
+	}
+ 
+	public void stop() {
+		
+		System.out.println("stopping engine...");
+	}
+  
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
 
 }

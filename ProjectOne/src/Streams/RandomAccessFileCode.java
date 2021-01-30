d23f392df0223d36a176c43f9c04a7416b4d6599
SaiTheFlyGuy <SaiTheFlyGuy@DESKTOP-Q4UN17C.myfiosgateway.com>
2021-01-29 20:35:05 -0500

    Initial Commit

---
+++ ProjectOne/src/Streams/RandomAccessFileCode.java
@@ -0,0 +1,44 @@
+package Streams;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+ 
+public class RandomAccessFileCode {
+ 
+    static final String FILEPATH = "Sai Bandreddi/git/ProjectOneTest/ProjectOne";
+ 
+    public static void main(String[] args) {
+ 
+        try {
+ 
+            System.out.println(new String(readFromFile(FILEPATH, 150, 23)));
+ 
+            writeToFile(FILEPATH, "Hello World!", 22);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+    private static byte[] readFromFile(String filePath, int position, int size)
+            throws IOException {
+ 
+        RandomAccessFile file = new RandomAccessFile(filePath, "r");
+        file.seek(position);
+        byte[] bytes = new byte[size];
+        file.read(bytes);
+        file.close();
+        return bytes;
+ 
+    }
+ 
+    private static void writeToFile(String filePath, String data, int position)
+            throws IOException {
+ 
+        RandomAccessFile file = new RandomAccessFile(filePath, "rw");
+        file.seek(position);
+        file.write(data.getBytes());
+        file.close();
+ 
+    }
+}
